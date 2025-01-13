3dff5dd19d6413165628cd08052a4baa5a1586b2
Kegrathil <willkegrathil@gmail.com>
2025-01-13 02:39:40 +0100

    hud fix for new ui layout

--- player/PlayerHUD.gd
+++ player/PlayerHUD.gd
@@ -1,13 +1,22 @@
 extends Node
 
 @onready var health_bar: TextureProgressBar = $HealthBar
-@onready var health: Health = $"../Health"
+@onready var player: Node3D = $"../ViewBox/SubViewport/DungeonTest/Player"
 
 
-func _on_health_took_damage() -> void:
-	update_health_bar()
+var health : Health
+
+func _ready():
+	if player == null:
+		push_error("UI can't find player")
+	else:
+		health = player.get_node("Health") as Health
+		if health == null:
+			push_error("Can't find health")
+		else:
+			health.took_damage.connect(update_health_bar)
 	
-func update_health_bar():
+func update_health_bar(_amount, _source):
 	var fill : float = health.cur_health as float/health.max_health as float
 	health_bar.set_value_no_signal(fill)
 	print("bar val: ", fill)
